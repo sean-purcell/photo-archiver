9cf521bf37d3effe6fd830caa8f180b14b4b1102
Sean Purcell <iburinoc@gmail.com>
2021-08-13 00:19:14 -0400

    Some skeleton setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use eyre::{Result, WrapErr};
+use structopt::StructOpt;
+use yup_oauth2::AccessToken;
+
+#[derive(Debug, StructOpt)]
+struct Auth {
+    #[structopt(short = "t", long = "token")]
+    token: String,
+}
+
+impl Auth {
+    fn access_token(&self) -> Result<AccessToken> {
+        let contents =
+            std::fs::read(&self.token).wrap_err(format!("Couldn't load file: {}", self.token))?;
+        let token = serde_json::from_slice::<AccessToken>(&contents)
+            .wrap_err(format!("Failed to parse token file: {}", self.token))?;
+        Ok(token)
+    }
+}
+
+#[derive(Debug, StructOpt)]
+enum Cmd {
+    List,
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "photo-archiver", about = "Archiver tool for google photos")]
+struct Args {
+    #[structopt(flatten)]
+    auth: Auth,
+
+    #[structopt(subcommand)]
+    cmd: Cmd,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = Args::from_args();
+
+    let token = args.auth.access_token()?;
+
+    match args.cmd {}
+
+    Ok(())
 }
